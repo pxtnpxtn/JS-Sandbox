9e3da78f9493d615963f313958bceeb4a532378d
Paxton <paxton.w.eicher@gmail.com>
2021-03-16 22:14:44 -0400

    Promises.js updated with fetch, promises and async await.

--- promises.js
+++ promises.js
@@ -1,69 +1,68 @@
-// Data
-const posts = [
-  { title: "Post One", body: "This is post one" },
-  { title: "Post Two", body: "This is post two" }
+// Fetch
+// Example: Simple Success
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+  .then(data => console.log('FETCH EXAMPLE', data));
+
+// Example: POST instead of GET (using the optional options argument)
+fetch("https://reqres.in/api/user", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    name: 'Paxton'
+  })
+})
+  .then(res => res.json())
+  .then(data => console.log('POST EXAMPLE-->', data));
+
+// Promises
+// 3 States: Pending, fulfilled, rejected.
+// Promise.all Example 1
+// Takes an array and waits until all promises have resolved.
+let urls = [
+  'https://api.github.com/users/iliakan',
+  'https://api.github.com/users/remy',
+  'https://api.github.com/users/jeresig'
 ];
 
-// Fake server response
-const getPosts = () => {
-  setTimeout(() => {
-    let output = "";
-    posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
-};
+let requests = urls.map(url => fetch(url));
 
-// Create new promise
-const createPost = post => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      posts.push(post);
+Promise.all(requests)
+  .then(responses => responses.forEach(
+    response => console.log('PROMISE.ALL EXAMPLE 1',(`${response.url}: ${response.status}`))
+  )
+);
 
-      const error = false;
+// Promise.all Example 2
+const promise1 = Promise.resolve("Resolved promise");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 2000, "Goodbye")
+);
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
+  res.json()
+);
 
-      if (!error) {
-        resolve();
-      } else {
-        reject("Error: something went wrong.");
-      }
-    }, 2000);
-  });
-};
-
-// createPost({ title: "Post Three", body: "This is post three" })
-//   .then(getPosts)
-//   .catch(err => console.log(err));
-
-// Promise.all
-// const promise1 = Promise.resolve("Resolved promise");
-// const promise2 = 10;
-// const promise3 = new Promise((resolve, reject) =>
-//   setTimeout(resolve, 2000, "Goodbye")
-// );
-// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
-//   res.json()
-// );
-
-// Promise.all([promise1, promise2, promise3, promise4]).then(values =>
-//   console.log(values)
-// );
+Promise.all([promise1, promise2, promise3, promise4]).then(values =>
+  console.log('PROMISE.ALL EXAMPLE 2', values)
+);
 
 // Async / Await
-// async function init() {
-//   await createPost({ title: "Post Three", body: "This is post three" });
-//   getPosts();
-// }
-// init();
+// "await" translates to .then (syntactic sugar)
 
-// Async / Await / Fetch
-async function fetchUsers() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-
-  const data = await res.json();
-
-  console.log(data);
+// async function fetchUsers() {
+const fetchUsers = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  
+    const data = await res.json();
+  
+    console.log('ASYNC AWAIT EXAMPLE -->', data);
+  } catch {
+    console.error(err);
+  }
 }
 
 fetchUsers();
